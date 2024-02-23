b0e6e76c27a1146523790a8a5a28d52001632e75
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2024-02-23 17:14:45 +0100

    something wrong with ssn.. maybe the active set?

--- src/SemismoothQVIs.jl
+++ src/SemismoothQVIs.jl
@@ -4,14 +4,16 @@ using Gridap, LineSearches, LinearAlgebra
 
 # include("models.jl")
 
-export GeneralizedThermoformingQVI,
-    fixed_point, semismoothnewton, moreau_yosida_newton, h1, EOC,
+export GeneralizedThermoformingQVI, NonlinearVI,
+    fixed_point, semismoothnewton, moreau_yosida_newton, visolver,
+    h1, EOC,
     fem_model,
     latex_table
 
 include("fem.jl")
 include("nls.jl")
 include("thermoforming.jl")
+include("nonlinearvi.jl")
 include("misc.jl")
 
 
